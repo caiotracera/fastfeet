d3971ca51fedd09548aed5916b29e9f0e35bd73f
Caio Tracera <ctrcra@gmail.com>
2020-11-02 22:16:22 -0300

    feat(users): add ShowProfile service and tests

---
+++ server/src/modules/users/services/ShowProfileService.spec.ts
@@ -0,0 +1,35 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ShowProfileService from '@modules/users/services/ShowProfileService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let showProfileService: ShowProfileService;
+
+describe('ShowProfileService', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    showProfileService = new ShowProfileService(fakeUsersRepository);
+  });
+
+  it('should be able to show de profile', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+      cpf: '99999999999',
+    });
+
+    const profile = await showProfileService.execute({ user_id: user.id });
+
+    expect(profile.name).toBe('John Doe');
+    expect(profile.email).toBe('johndoe@example.com');
+  });
+
+  it('should not be able to show the profile from a non-existing user', async () => {
+    await expect(
+      showProfileService.execute({ user_id: 'non-existing-id' }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
