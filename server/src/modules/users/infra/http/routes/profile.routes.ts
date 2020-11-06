3edef8fb0ee6dfcd315d7f665b798871b61234dc
Caio Tracera <ctrcra@gmail.com>
2020-11-06 16:26:19 -0300

    feat(users): add profile route

---
+++ server/src/modules/users/infra/http/routes/profile.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { Segments, Joi, celebrate } from 'celebrate';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import ProfileController from '@modules/users/infra/http/controllers/ProfileController';
+
+const profileRouter = Router();
+const profileController = new ProfileController();
+
+profileRouter.use(ensureAuthenticated);
+profileRouter.get('/', profileController.show);
+
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      cpf: Joi.string().required().min(11).max(11),
+      deliveryman: Joi.bool().required(),
+      old_password: Joi.string(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  profileController.update,
+);
