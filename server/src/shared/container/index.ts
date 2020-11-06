f578a6d368a04ce7537f7c080a13138b8e9f4d1a
Caio Tracera <ctrcra@gmail.com>
2020-11-06 18:50:10 -0300

    feat(providers): add DeliveriesRepository to providers

--- server/src/shared/container/index.ts
+++ server/src/shared/container/index.ts
@@ -9,6 +9,9 @@ import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepo
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
+import IDeliveriesRepository from '@modules/deliveries/repositories/IDeliveriesRepository';
+import DeliveriesRepository from '@modules/deliveries/infra/typeorm/repositories/DeliveriesRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
@@ -17,4 +20,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IUserTokensRepository>(
   'UserTokensRepository',
   UserTokensRepository,
+);
+
+container.registerSingleton<IDeliveriesRepository>(
+  'DeliveriesRepository',
+  DeliveriesRepository,
 );
