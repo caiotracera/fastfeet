62923aac5d56cf32e9479f0c351487dc25292310
Caio Tracera <ctrcra@gmail.com>
2020-11-06 12:49:25 -0300

    feat(users): add update avatar route to users routes

--- server/src/modules/users/infra/http/routes/users.routes.ts
+++ server/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { Segments, celebrate, Joi } from 'celebrate';
 
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import UsersController from '@modules/users/infra/http/controllers/UsersController';
+import UserAvatarController from '@modules/users/infra/http/controllers/UserAvatarController';
+
+import uploadConfig from '@config/upload';
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig.multer);
 
 usersRouter.post(
   '/',
@@ -17,4 +25,11 @@ usersRouter.post(
     },
   }),
   usersController.create,
+);
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
 );
