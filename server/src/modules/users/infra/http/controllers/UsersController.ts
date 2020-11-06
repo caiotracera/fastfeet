02319757e46b1e9902fb67c593feaea83f679239
Caio Tracera <ctrcra@gmail.com>
2020-11-06 11:59:29 -0300

    feat(users): add UsersController

---
+++ server/src/modules/users/infra/http/controllers/UsersController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
+
+import AppError from '@shared/errors/AppError';
+
+import CreateUserService from '@modules/users/services/CreateUserService';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+
+export default class UsersController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    try {
+      const { name, email, cpf, password } = request.body;
+
+      const createUser = container.resolve(CreateUserService);
+      const user = await createUser.execute({ name, email, password, cpf });
+
+      return response.json(classToClass(user));
+    } catch (error) {
+      if (error instanceof AppError) {
+        return response.status(error.statusCode).json({ error: error.message });
+      }
+      return response.status(400).json({ error: error.message });
+    }
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    return response.json(classToClass(user));
+  }
+}
