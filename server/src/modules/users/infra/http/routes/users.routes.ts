b9ac74da02a1bf435f1de6c6700b7e73bc4c2d96
Caio Tracera <ctrcra@gmail.com>
2020-11-06 12:43:56 -0300

    feat(users): add Users routes

---
+++ server/src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { Segments, celebrate, Joi } from 'celebrate';
+
+import UsersController from '@modules/users/infra/http/controllers/UsersController';
+
+const usersRouter = Router();
+const usersController = new UsersController();
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      cpf: Joi.string().required().min(11).max(11),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
