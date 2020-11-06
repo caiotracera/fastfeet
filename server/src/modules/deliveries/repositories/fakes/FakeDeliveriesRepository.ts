e825b57f3a0464bd642b6c14f94e2d66e9f97f3b
Caio Tracera <ctrcra@gmail.com>
2020-11-06 18:44:59 -0300

    feat(deliveries): add FakeDeliveriesRepository

---
+++ server/src/modules/deliveries/repositories/fakes/FakeDeliveriesRepository.ts
@@ -0,0 +1,60 @@
+import { v4 as uuid } from 'uuid';
+import { getDate, getMonth, getYear } from 'date-fns';
+
+import ICreateDeliveryDTO from '@modules/deliveries/dtos/ICreateDeliveryDTO';
+import IFindAllInDayFromDeliverymanDTO from '@modules/deliveries/dtos/IFindAllInDayFromDeliverymanDTO';
+import IDeliveriesRepository from '@modules/deliveries/repositories/IDeliveriesRepository';
+import Delivery from '@modules/deliveries/infra/typeorm/entities/Delivery';
+
+export default class FakeDeliveriesRepository implements IDeliveriesRepository {
+  private deliveries: Delivery[] = [];
+
+  public async create({
+    product,
+    address,
+    city,
+    neighborhood,
+    postal_code,
+    state,
+  }: ICreateDeliveryDTO): Promise<Delivery> {
+    const delivery = new Delivery();
+
+    Object.assign(delivery, {
+      id: uuid(),
+      product,
+      address,
+      city,
+      neighborhood,
+      postal_code,
+      state,
+    });
+
+    this.deliveries.push(delivery);
+    return delivery;
+  }
+
+  public async findById(delivery_id: string): Promise<Delivery | undefined> {
+    const delivery = this.deliveries.find(
+      eachDelivery => eachDelivery.id === delivery_id,
+    );
+
+    return delivery;
+  }
+
+  public async findAllInDayFromDeliveryman({
+    deliveryman_id,
+    day,
+    month,
+    year,
+  }: IFindAllInDayFromDeliverymanDTO): Promise<Delivery[]> {
+    const deliveries = this.deliveries.filter(
+      delivery =>
+        delivery.deliveryman_id === deliveryman_id &&
+        getDate(delivery.start_date) === day &&
+        getMonth(delivery.start_date) === month &&
+        getYear(delivery.start_date) === year,
+    );
+
+    return deliveries;
+  }
+}
