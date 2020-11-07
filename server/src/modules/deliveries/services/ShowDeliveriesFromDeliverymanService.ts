5dcbce7e65cb393f618d1bc0a4f8cc9a18bb6d77
Caio Tracera <ctrcra@gmail.com>
2020-11-06 23:00:27 -0300

    feat(deliveries): add ShowDeliveriesFromDeliveryman service and tests

---
+++ server/src/modules/deliveries/services/ShowDeliveriesFromDeliverymanService.ts
@@ -0,0 +1,38 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IDeliveriesRepository from '@modules/deliveries/repositories/IDeliveriesRepository';
+import Delivery from '@modules/deliveries/infra/typeorm/entities/Delivery';
+
+interface IRequest {
+  deliveryman_id: string;
+}
+
+@injectable()
+export default class ShowDeliveriesFromDeliveryman {
+  constructor(
+    @inject('DeliveriesRepository')
+    private deliveriesRepository: IDeliveriesRepository,
+
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ deliveryman_id }: IRequest): Promise<Delivery[]> {
+    const user = await this.usersRepository.findById(deliveryman_id);
+    if (!user) {
+      throw new AppError('Deliveryman not found', 404);
+    }
+
+    if (!user.deliveryman) {
+      throw new AppError('User is not a deliveryman');
+    }
+
+    const deliveries = await this.deliveriesRepository.findByDeliveryman(
+      deliveryman_id,
+    );
+
+    return deliveries;
+  }
+}
