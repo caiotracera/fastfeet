83ab07a81604c8ccf5bc2974057423a2dd164d31
Caio Tracera <ctrcra@gmail.com>
2020-11-14 22:41:34 -0300

    feat(tests): add ListUnfinishedDeliveries and ListFinishedDeliveries services and tests

---
+++ server/src/modules/deliveries/services/ListFinishedDeliveriesService.spec.ts
@@ -0,0 +1,94 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeDeliveriesRepository from '@modules/deliveries/repositories/fakes/FakeDeliveriesRepository';
+import ListFinishedDeliveriesService from '@modules/deliveries/services/ListFinishedDeliveriesService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeDeliveriesRepository: FakeDeliveriesRepository;
+let listFinishedDeliveriesService: ListFinishedDeliveriesService;
+
+describe('ListUnfinishedDeliveries', () => {
+  beforeEach(() => {
+    fakeDeliveriesRepository = new FakeDeliveriesRepository();
+    fakeUsersRepository = new FakeUsersRepository();
+
+    listFinishedDeliveriesService = new ListFinishedDeliveriesService(
+      fakeDeliveriesRepository,
+      fakeUsersRepository,
+    );
+  });
+
+  it('should be able to list all the unfinished deliveries from deliveryman', async () => {
+    const deliveryman = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+      cpf: '99999999999',
+      deliveryman: true,
+    });
+
+    const delivery1 = await fakeDeliveriesRepository.create({
+      address: 'Random address',
+      city: 'Random city',
+      neighborhood: 'Random neighborhood',
+      state: 'Random State',
+      postal_code: '99999999',
+      product: 'Random product',
+      deliveryman_id: deliveryman.id,
+    });
+
+    delivery1.end_date = new Date();
+    await fakeDeliveriesRepository.save(delivery1);
+
+    await fakeDeliveriesRepository.create({
+      address: 'Random address',
+      city: 'Random city',
+      neighborhood: 'Random neighborhood',
+      state: 'Random State',
+      postal_code: '99999999',
+      product: 'Random product',
+      deliveryman_id: deliveryman.id,
+    });
+
+    await fakeDeliveriesRepository.create({
+      address: 'Random address',
+      city: 'Random city',
+      neighborhood: 'Random neighborhood',
+      state: 'Random State',
+      postal_code: '99999999',
+      product: 'Random product',
+      deliveryman_id: deliveryman.id,
+    });
+
+    const deliveries = await listFinishedDeliveriesService.execute({
+      deliveryman_id: deliveryman.id,
+    });
+
+    expect(deliveries).toEqual([delivery1]);
+  });
+
+  it('should not be able to list the deliveries from a non-existing deliveryman', async () => {
+    await expect(
+      listFinishedDeliveriesService.execute({
+        deliveryman_id: 'non-existing-deliveryman',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to list the deliveries from a non-deliveryman user', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+      cpf: '99999999999',
+      deliveryman: false,
+    });
+
+    await expect(
+      listFinishedDeliveriesService.execute({
+        deliveryman_id: user.id,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ server/src/modules/deliveries/services/ListFinishedDeliveriesService.ts
@@ -0,0 +1,44 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IDeliveriesRepository from '@modules/deliveries/repositories/IDeliveriesRepository';
+import Delivery from '@modules/deliveries/infra/typeorm/entities/Delivery';
+
+interface IRequest {
+  deliveryman_id: string;
+}
+
+@injectable()
+export default class ListUnfinishedDeliveriesService {
+  constructor(
+    @inject('DeliveriesRepository')
+    private deliveriesRepository: IDeliveriesRepository,
+
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ deliveryman_id }: IRequest): Promise<Delivery[]> {
+    const user = await this.usersRepository.findById(deliveryman_id);
+    if (!user) {
+      throw new AppError('Deliveryman not found', 404);
+    }
+
+    if (!user.deliveryman) {
+      throw new AppError('User is not a deliveryman');
+    }
+
+    const deliveries = await this.deliveriesRepository.findByDeliveryman(
+      deliveryman_id,
+    );
+
+    console.log(deliveries);
+
+    const finished = deliveries.filter(
+      delivery => delivery.end_date !== undefined,
+    );
+
+    return finished;
+  }
+}
