c8339c2a69d5eb51f6b827e219224f0446614077
Caio Tracera <ctrcra@gmail.com>
2020-11-17 12:02:26 -0300

    refactor(deliveries): redoing deliveries module

--- server/src/modules/deliveries/dtos/ICreateDeliveryDTO.ts
+++
@@ -1,9 +0,0 @@
-export default interface ICreateDeliveryDTO {
-  product: string;
-  address: string;
-  postal_code: string;
-  neighborhood: string;
-  city: string;
-  state: string;
-  deliveryman_id?: string;
-}
