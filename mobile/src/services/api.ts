7f4ece2b47abd5d7d6874201d97121aece77e63e
Caio Tracera <ctrcra@gmail.com>
2020-11-17 18:39:13 -0300

    feat(api): add interceptors

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.25.27:3333',
+  baseURL: 'http://192.168.25.28:3333',
 });
 
 api.interceptors.response.use(
@@ -10,9 +10,27 @@ api.interceptors.response.use(
     return response;
   },
   async (error) => {
-    const token = AsyncStorage.getItem('@FastFeet:token');
-    if (error.response.status === 401 && token) {
-      console.log(error);
+    const refresh_token = await AsyncStorage.getItem('@FastFeet:token');
+    const user = await AsyncStorage.getItem('@FastFeet:user');
+
+    if (user) {
+      const parsedUser = JSON.parse(user);
+
+      if (
+        error.response.status === 401 &&
+        refresh_token &&
+        parsedUser.remember
+      ) {
+        const response = await axios.put('sessions', {
+          grant_type: 'refresh_token',
+          refresh_token,
+        });
+
+        const { token } = response.data;
+        await AsyncStorage.setItem('@FastFeet:token', token);
+
+        return Promise.resolve();
+      }
     }
     return Promise.reject(error);
   },
